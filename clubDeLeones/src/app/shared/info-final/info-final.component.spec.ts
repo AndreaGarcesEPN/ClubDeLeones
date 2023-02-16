fac881771d0484c0009556e67352c7c6ccd41016
Andrea_Garces <50632356+AndreaGarcesEPN@users.noreply.github.com>
2023-02-16 17:26:03 -0500

    info-final

---
+++ clubDeLeones/src/app/shared/info-final/info-final.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoFinalComponent } from './info-final.component';
+
+describe('InfoFinalComponent', () => {
+  let component: InfoFinalComponent;
+  let fixture: ComponentFixture<InfoFinalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ InfoFinalComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InfoFinalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ clubDeLeones/src/app/shared/info-final/info-final.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-final',
+  templateUrl: './info-final.component.html',
+  styleUrls: ['./info-final.component.css']
+})
+export class InfoFinalComponent {
+
+}
